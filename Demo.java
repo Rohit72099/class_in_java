61a15aef2e6f5f26cb179810b54e8a1ab9e869e9
Rohit72099 <112176838+Rohit72099@users.noreply.github.com>
2024-03-17 00:45:23 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,112 @@
+//package java_cse310;
+//import java.math.*;
+//import java.lang.*;
+//
+//
+//public class Demo {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+////		BigDecimal x= new BigDecimal("65465.000004545424");
+////		System.out.println(x);
+//		//ques1.
+//		
+////		BigDecimal b1 = new BigDecimal("2.01");
+////		BigDecimal b2 = new BigDecimal("1.010965"); 
+//		
+////		BigDecimal result= b1.add(b2).setScale(3, RoundingMode.HALF_UP);
+////		System.out.println(result);
+//		
+////		BigDecimal result = b1.multiply(b2);
+////		System.out.println(b1.multiply(b2));
+////		
+//		//ques2.
+//		
+//		
+//		
+//		
+//		
+//		
+////		char ch1='a';
+////		char ch2='9';
+////		char ch3='*';
+////		System.out.print("ch1 is character : " + Character.isLetter(ch1));
+////		Character.getNumericValue
+////		if(Character.isDigit(ch1)) {
+//////			System.out.println(ch1);
+//////			
+//////		}
+//////		else if(Character.isLetter(ch1)) {
+//////			System.out.println(Character.toUpperCase(ch1));
+//////		}
+//////		else {
+//////			System.out.println("Invalid");
+//////		}
+////		char ch = Character.toLowerCase(ch1);
+//		System.out.println(ch);
+////		
+//////		
+////		int g =23;
+////		Integer y = new Integer (g);
+////		System.out.println(y);
+//		
+//		
+//		
+////		byte b=23;
+////		b=b+1;
+////		System.out.println(b);
+//		
+////	double d=5/0;
+////	System.out.println(d);
+//		
+//		//parse method is used to fetch primitive value from string
+//		String x="4";
+////		int y=Integer.parseInt("4");
+//		
+//		
+//		
+//		Integer y = Integer.valueOf(x);
+//		System.out.println(y);
+//		
+//		//unary operator: - 
+//		
+//		
+//		
+//		//1. all byte ,short and char values are promoted to int ;
+////		2. if one operand is a long , the whole expression is promoted to long;
+//		
+//		
+//		
+//		System.out.println(35>>2);//right shift
+//		System.out.println(35<<2);//left shift
+//		System.out.println(-1>>>24);//unsigned right shift
+//		System.out.println(-1>>24);//right shift
+//		
+//		
+//		//ternary operator
+////		boolean b = args instanceof String[];
+////		boolean b = args instanceof ;  //doubt
+//		
+//		System.out.println("ganga" +2+3);
+//		System.out.println(3+ "ganga"+true+'R');
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//				
+//
+//	}
+//
+//}
